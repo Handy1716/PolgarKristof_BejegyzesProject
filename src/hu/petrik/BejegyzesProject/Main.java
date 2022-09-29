19b883e610cec1a248c7b06200c9b3c0c0df37df
Polgár Kristóf <polgarkristof@gmail.com>
2022-09-29 08:46:21 +0200

    próbálozás a toStringel

--- src/hu/petrik/BejegyzesProject/Main.java
+++ src/hu/petrik/BejegyzesProject/Main.java
@@ -1,7 +1,11 @@
 package hu.petrik.BejegyzesProject;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        Bejegyzes bejegyzes1 = new Bejegyzes("Jancsi", "fát vágni jó");
+        System.out.println(bejegyzes1);
     }
 }
