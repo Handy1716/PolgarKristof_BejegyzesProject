db5ca521758fb3a1aa87652687ce1ba70e83e383
Polgár Kristóf <polgarkristof@gmail.com>
2022-09-29 09:37:46 +0200

    bejegyzesekLista

--- src/hu/petrik/BejegyzesProject/Main.java
+++ src/hu/petrik/BejegyzesProject/Main.java
@@ -1,13 +1,19 @@
 package hu.petrik.BejegyzesProject;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        List<Bejegyzes> bejegyzesek = new ArrayList<>();
+
         Scanner sc = new Scanner(System.in);
         Bejegyzes bejegyzes1 = new Bejegyzes("Jancsi", "fát vágni jó");
-        System.out.println(bejegyzes1);
+        bejegyzesek.add(bejegyzes1);
         bejegyzes1.setTartalom("dadsadsadad");
-        System.out.println(bejegyzes1);
+        Bejegyzesek bejegyzesLista = new Bejegyzesek("bejegyzesek.csv");
+        System.out.println(bejegyzesLista);
     }
 }
