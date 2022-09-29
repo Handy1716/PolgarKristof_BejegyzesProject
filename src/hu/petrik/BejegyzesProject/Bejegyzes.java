a956cb0919e35af5ddabc7d6a81e210510e81dff
Polgár Kristóf <polgarkristof@gmail.com>
2022-09-29 08:41:54 +0200

    gitignore

--- src/hu/petrik/BejegyzesProject/Bejegyzes.java
+++ src/hu/petrik/BejegyzesProject/Bejegyzes.java
@@ -11,11 +11,50 @@ public class Bejegyzes {
         this.szerkesztve = LocalDateTime.now();
     }
 
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
+    }
+
     private String szerzo;
+
+    public void setTartalom(String tartalom) {
+        this.szerkesztve = LocalDateTime.now();
+        this.tartalom = tartalom;
+    }
+
+    public void Like(){
+        this.likeok++;
+    }
+
     private String tartalom;
     private int likeok;
     private LocalDateTime letrejott;
     private LocalDateTime szerkesztve;
 
-
+    @Override
+    public String toString() {
+        return "Bejegyzes{" +
+                "szerzo='" + szerzo + '\'' +
+                ", tartalom='" + tartalom + '\'' +
+                ", likeok=" + likeok +
+                ", letrejott=" + letrejott +
+                ", szerkesztve=" + szerkesztve +
+                '}';
+    }
 }
